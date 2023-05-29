0b390c698697c606fed7a794d3cf09abaec56517
YohannesAH <yohannesassefa49@gmail.com>
2023-05-29 20:36:46 +0300

    A flask app for local testing and ready to be deployed in minikube

---
+++ app3.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify, request
+import requests
+import uuid
+import json
+import os
+
+app = Flask(__name__)
+
+key = os.environ.get('API_KEY')
+endpoint = os.environ.get('ENDPOINT')
+location = "northeurope"
+
+@app.route('/translate', methods=['POST'])
+def translate():
+    path = '/translate'
+    constructed_url = endpoint + path
+
+    params = {
+        'api-version': '3.0',
+        'from': request.json.get('from_language', 'en'),
+        'to': request.json.get('to_languages', [])
+    }
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': key,
+        'Ocp-Apim-Subscription-Region': location,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    body = [{
+        'text': request.json.get('text', '')
+    }]
+
+    response = requests.post(constructed_url, params=params, headers=headers, json=body)
+    response_data = response.json()
+
+    return jsonify(response_data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002)
